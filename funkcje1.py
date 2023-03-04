3ce324088f9e6ffb2d04b4f4d6abf86b9cbf5782
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-03-04 10:54:34 +0100

    Python3

---
+++ funkcje1.py
@@ -0,0 +1,15 @@
+
+from funkcje2_kod import *
+
+imie = 'kasia'
+print(len(imie))
+number = 233.434
+    print(round(number, 3))
+
+    welcome_basic()
+    imie1 = "Marysia"
+    lista_imion =['Adam', 'Marek']
+
+    welcome_full('Waldemar', 38)
+    welcome_full(imie1, 12)
+    welcome_full(lista_imion[1], 94)
---
+++ funkcje2.py
@@ -0,0 +1,14 @@
+import time
+def welcome_basic()
+    time.sleep(1.5)
+    print('przygotowaie obrazu systemu')
+    time.sleep(1.5)
+    print('sprawdzanie bedow w systemie')
+    time.sleep(1.5)
+    print('siema')
+
+def welcome_full(imie, wiek)
+    if wiek >= 18:
+        print('Dzien dobry', imie)
+    else:
+        print('Czesc', imie)
